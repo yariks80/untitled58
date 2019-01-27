da37961305435a5f7e1fb7c2565e09cab0f0987a
yarik <yariks@i.ua>
2019-01-27 10:16:10 +0200

    commit0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,15 @@ public class Main {
         System.out.println("hi");
         System.out.println("hi");
         System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+
 
 
     }
