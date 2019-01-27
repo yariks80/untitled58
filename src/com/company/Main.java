7f30044f54aa2ef21826463bc96c961bd701e8bb
yarik <yariks@i.ua>
2019-01-27 09:30:41 +0200

    commit1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,16 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code
+        System.out.println("hello");
         System.out.println("hi");
         System.out.println("hi");
         System.out.println("hi");
-        System.out.println("hi");
-        System.out.println("hi");
-        System.out.println("hi");
-        System.out.println("hi");
-        System.out.println("hi");
-        System.out.println("hi");
-        System.out.println("hi");
+
 
 
     }
